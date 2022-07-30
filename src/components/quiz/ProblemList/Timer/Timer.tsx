04b41b0d2244a592e8c5b082df368f51dcff7719
parkjeyoung <dkfmwpsxm@naver.com>
2022-07-31 00:30:34 +0900

    Feat L timer 컴포넌트 작성

---
+++ src/components/quiz/ProblemList/Timer/Timer.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useCallback } from "react";
+import styled from "styled-components";
+import { useInterval } from "../../../../utils/CustomHook";
+
+const Container = styled.span`
+  font-size: 17px;
+  span {
+    font-size: 16px;
+  }
+`;
+
+function Timer() {
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [hour, setHour] = useState<number>(0);
+
+  const count = useCallback(() => {
+    setSeconds((sec) => {
+      if (sec > 59) {
+        setMinutes((min) => {
+          if (min > 59) {
+            setHour((h) => h + 1);
+            return 0;
+          } else {
+            return min + 1;
+          }
+        });
+        return 0;
+      } else {
+        return sec + 1;
+      }
+    });
+  }, [setMinutes, setSeconds, setHour]);
+
+  useInterval(count, 1000);
+
+  return (
+    <Container>
+      <span>{`경과시간: `}</span>
+      {hour < 10 ? `0${hour}` : hour}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+    </Container>
+  );
+}
+
+export default Timer;
