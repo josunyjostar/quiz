ec0d5d6b80e6ad59304cfd006ddaed1506915b81
parkjeyoung <dkfmwpsxm@naver.com>
2022-07-30 23:39:56 +0900

    Feat : 커스텀 훅 추가 (useInterval)

---
+++ src/utils/CustomHook.tsx
@@ -0,0 +1,23 @@
+import { useRef, useEffect } from "react";
+
+export function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef<() => void>(() => {
+    console.log("useInterval");
+  });
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
