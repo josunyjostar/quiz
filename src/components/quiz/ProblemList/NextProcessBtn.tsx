3db1de78c3d3841d40a0c31ab068f168ebdb9e4f
parkjeyoung <dkfmwpsxm@naver.com>
2022-07-30 23:04:05 +0900

    Feat : NextProcessBtn 컴포넌트 작성 3차

--- src/components/quiz/ProblemList/NextProcessBtn.tsx
+++ src/components/quiz/ProblemList/NextProcessBtn.tsx
@@ -21,20 +21,29 @@ const Container = styled(Btn)<StyledProps>`
   button:last-child {
     visibility: ${(props) => (props.isOpenAnswer ? "visible" : "hidden")};
   }
+
+  button.end {
+    background-color: #d63031;
+  }
+  button:hover.end {
+    background-color: #c23616;
+  }
 `;
 
 interface Props {
   isSelected: boolean;
   isOpenAnswer: boolean;
+  isEnd: boolean;
   openAnswer: () => void;
+  nextProblem: () => void;
 }
 
-function NextProcessBtn({ isSelected, openAnswer, isOpenAnswer }: Props) {
+function NextProcessBtn({ isSelected, openAnswer, isOpenAnswer, nextProblem, isEnd }: Props) {
   return (
     <Container isSelected={isSelected} isOpenAnswer={isOpenAnswer}>
       <button></button>
       <button onClick={openAnswer}>정답 확인</button>
-      <button>다음 문제</button>
+      {isEnd ? <button className="end">최종제출</button> : <button onClick={nextProblem}>다음 문제</button>}
     </Container>
   );
 }
