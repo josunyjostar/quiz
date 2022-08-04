8e1315366cfddd377c5a260dc361d4e7bd5360c5
parkjeyoung <dkfmwpsxm@naver.com>
2022-08-04 20:14:52 +0900

    Test : NextProcessBtn test case 추가 2차

--- src/components/quiz/ProblemList/NextProcessBtn/test/NextProcessBtn.test.tsx
+++ src/components/quiz/ProblemList/NextProcessBtn/test/NextProcessBtn.test.tsx
@@ -1,23 +1,37 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, logRoles } from "@testing-library/react";
 import NextProcessBtn, { Props } from "../NextProcessBtn";
 
 describe("<NextProcessBtn />", () => {
-  function componentRender() {
-    const initVal: Props = {
-      isSelected: false,
-      isOpenAnswer: false,
-      isEnd: false,
-      openAnswer: jest.fn(),
-      nextProblem: jest.fn(),
-      submitTest: jest.fn(),
-    };
-    render(<NextProcessBtn {...initVal} />);
+  const props: Props = {
+    isSelected: false,
+    isOpenAnswer: false,
+    isEnd: false,
+    openAnswer: jest.fn(),
+    nextProblem: jest.fn(),
+    submitTest: jest.fn(),
+  };
+  function componentRender(val: Props) {
+    return render(<NextProcessBtn {...val} />);
   }
 
-  it("confirm btn is hidden when answer is not checked", () => {
-    componentRender();
-    const btn = screen.getByText(/정답 확인/);
-    expect(btn).not.toBeVisible();
+  it("btn is hidden when answer is not checked", () => {
+    componentRender(props);
+    const confirmBtn = screen.getByText(/정답 확인/);
+    const nextBtn = screen.getByText(/다음 문제/);
+    expect(confirmBtn).not.toBeVisible();
+    expect(nextBtn).not.toBeVisible();
+  });
+
+  it("confirmBtn is visible when answer is selected", () => {
+    componentRender({ ...props, isSelected: true });
+    const confirmBtn = screen.getByText(/정답 확인/);
+    expect(confirmBtn).toBeVisible();
+  });
+
+  it("nextBtn is visible when answer is selected", () => {
+    componentRender({ ...props, isOpenAnswer: true });
+    const nextBtn = screen.getByText(/다음 문제/);
+    expect(nextBtn).toBeVisible();
   });
 });
